1af1e57c93424e4003783247b8fa6cc4bab3e3a0
MateuszKociolek <mkociolek2005@gmail.com>
2023-05-19 10:34:26 +0200

    funckja logowania

--- expr.js
+++ expr.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var path = require("path")
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -8,6 +9,18 @@ app.get("/", (req, res) => {
 app.get('/users/:id', (req, res) => {
     const userId = req.params.id;
     res.send(`Hello user: ${userId}`);
+});
+
+app.get("/login/:username/:password", (req, res) => {
+    const username = req.params.username;
+    const password = req.params.password;
+    if(username == "koci" && password == "haslo0"){
+        console.log("Zalogowano")
+        res.sendFile(path.join(__dirname, "./zalogowanie.html"))
+        // res.send(`Hello user ${username}`);
+    }else{
+        res.send("Invalid username or password");
+    }
 });
 
 app.listen(3000 , () => {
---
+++ script.js
@@ -0,0 +1,22 @@
+// document.getElementById("myForm").addEventListener('submit', 
+function signin() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    console.log(`Username: ${username}, Password: ${password}`)
+    fetch(`http://localhost:3000/login/${username}/${password}`)
+    .then(function(res){
+        if(res.ok) {
+            console.log("Wszystko ok")
+            return res.json()
+        }else{
+            throw new Error("Wystąpił błąd sieciowy.")
+        }
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+    .catch(function(error) {
+        console.log(error)
+    })
+};
