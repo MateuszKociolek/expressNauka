6598d70e5b808c31d17396be3d17c438404ae8e0
MateuszKociolek <mkociolek2005@gmail.com>
2023-05-21 21:51:51 +0200

    dodanie bazy i problem z logowaniem dwa razy

--- expr.js
+++ expr.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
-var path = require("path")
+var mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'konta'
+});
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -8,27 +15,67 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
 });
 
-app.get('/users/:id', (req, res) => {
-    const userId = req.params.id;
-    res.send(`Hello user: ${userId}`);
+app.get('/allusers', (req, res) => {
+    connection.connect((error) => {
+        if(error){
+            console.log('Error with connection:' + error);
+            return;
+        }
+        console.log("Connected to db");
+
+        const query = 'select * from users';
+
+        connection.query(query, (err, results, fields) => {
+            if(err){
+                console.log("Error with query");
+                return res.status(500).json({error: 'Server error'});
+            }
+            console.log("Show all users: OK");
+            res.json(results);
+        })
+    });
 });
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     console.log(username, password);
-    if(username == "koci" && password == "haslo0"){
-        console.log("Zalogowano");
-        // res.send("All good!");
-        res.sendFile(__dirname + '/zalogowanie.html');
-        // res.send(`Hello user ${username}`);
-    }else{
-        console.log("złe dane");
-        res.send("Invalid username or password");
-    }
+
+    connection.connect((error) => {
+        if(error){
+            console.log("Error connection to db with users");
+            console.log(error)
+            return;
+        }
+        
+        const query = `select username, password from users where username = '${username}' and password = '${password}'`;
+
+        connection.query(query, (err, results, fields) => {
+            if(err){
+                console.log("Error with login to users");
+                return res.status(500).json({error:"error with users"});
+            }
+            if (results != 0) {
+                console.log("Pomyślnie zalogowano");
+                res.sendFile(__dirname + '/zalogowanie.html');
+            }else{
+                console.log("Złe logowanie")
+            }
+            
+            connection.end();
+        })
+    })
+
+
+    // if(username == "koci" && password == "haslo0"){
+    //     console.log("Zalogowano");
+    //     res.sendFile(__dirname + '/zalogowanie.html');
+    // }else{
+    //     console.log("złe dane");
+    //     res.send("Invalid username or password");
+    // }
 });
 
 app.listen(3000 , () => {
     console.log("Serwer start at port 3000");
 });
-``
